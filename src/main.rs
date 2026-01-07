97d0f54e01fe1a3664b6b62fbc2ddc4f170695e8
Amumug <fabriali4@gmail.com>
2026-01-07 23:51:44 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -8,18 +8,10 @@ use core::panic::PanicInfo;
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
-static HELLO: &[u8] = b"Hello World!";
 
-#[unsafe(no_mangle)]
+#[unsafe(no_mangle)]// don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    // This function is the entry point, since the linker looks for a function
+    // name `_start` by default
     loop {}
 }
