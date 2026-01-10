0840277793039cee30bba897b889ab39cbd29787
Amumug <fabriali4@gmail.com>
2026-01-10 22:14:44 +0100

    vga text mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]// don't link the Rust standard library
 #![no_main]// disable all Rust-level entry points
-
+mod vga_buffer;
 use core::panic::PanicInfo;
 
 // This function is called on panic.
@@ -26,3 +26,4 @@ pub extern "C" fn _start() -> ! {
 
     loop {}
 }
+
